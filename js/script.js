b3f08747d0196a9027888ca61d54f635d60742ff
Alioune <alioune1117@gmail.com>
2020-06-29 21:01:47 +0200

    parzialmente completato.

--- js/script.js
+++ js/script.js
@@ -20,6 +20,7 @@ $(document).ready(function(){
   // Lo visualizzo nell'html
   $('#month_displayed').html(monthInHtml + ' ' + year);
 
+  console.log(monthInHtml);
 
   // TEMPLATE handlebars
 
@@ -51,6 +52,102 @@ $(document).ready(function(){
     $('.calendar_container').append(html);
 
   }
+
+  console.log(currentMonth);
+
+  nDays = moment(initialDate).format('MMMM dddd');
+
+  console.log(initialDate);
+
+
+  // FACCIO LA CHIAMATA AJAX ALL'API BOOLEAN PER OTTENERE LE FESTIVITA
+
+  var holidays = $.ajax(
+    {
+      url:"https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
+      method: "GET",
+
+      // SE LA CHIAMATA HA SUCCESSO:
+
+      success: function(request){
+
+        var dataRequest = request.response;
+        // console.log(dataRequest);
+        var  arrayCalendar = [];
+        var  arrayRequest = [];
+
+        for (var j = 0; j < dataRequest.length; j++) {
+
+          var singleElement = dataRequest[j];
+
+          var dateInRequest = singleElement.date;
+
+          var nameHoliday = singleElement.name;
+
+          var dayInRequest = moment(dateInRequest).day();
+
+          arrayRequest.push(dayInRequest)
+
+          console.log(dayInRequest);
+          console.log(nameHoliday);
+
+          for (var a = 0; a < currentMonth.length; a++) {
+            var currentMonthElement = currentMonth[a];
+            var dayInCurrentMonth = currentMonthElement.dayNumber;
+
+            arrayCalendar.push(dayInCurrentMonth);
+
+            var control = false;
+
+            if (arrayCalendar.includes(dayInRequest)) {
+              var control = true;
+
+
+            }
+
+          }
+
+          if (control) {
+            $('.calendar_container').find('li').append(nameHoliday);
+          }
+
+
+        }
+
+
+
+
+
+
+        console.log(control);
+        console.log(arrayCalendar);
+
+
+        console.log(dayInCurrentMonth);
+        //
+        // if (control) {
+        //   console.log("sono nella if");
+        // }
+
+
+
+
+
+
+
+
+
+      },
+
+
+      // SE LA CHIAMATA DOVESSE FALLIRE
+      // visualizzo un errore
+      error: function(){
+        alert("Si è verificato un errore");
+      }
+
+  }
+  )
 
 
 

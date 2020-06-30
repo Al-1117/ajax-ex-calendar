5485bab86319a3884177421d3333e6c608e50da5
Alioune <alioune1117@gmail.com>
2020-06-30 15:40:34 +0200

    fatto delle correzioni, manca ancora l'evidenziazione delle festività

--- js/script.js
+++ js/script.js
@@ -3,186 +3,125 @@ $(document).ready(function(){
  // Imposto la variabile della data iniziale del calendario (1 gennaio 2018)
   var initialDate = moment("2018-01-01");
 
-  // Recupro il mese corrente
-  var month = moment(initialDate).month();
+  displayMonth(initialDate);
 
-  // Calcolo quanto giorni ci sono nel mese
-  var daysMonth = moment(month).daysInMonth();
-
-  var year = moment(initialDate).year();
-
-  console.log(year);
-
-  //Setto il formato del mese da visualizzare nell'html
-
-  var monthInHtml = moment(initialDate).format("MMMM");
-
-  // Lo visualizzo nell'html
-  $('#month_displayed').html(monthInHtml + ' ' + year);
-
-  console.log(monthInHtml);
-
-  // TEMPLATE handlebars
-
-  var source = $('#calendar_template').html();
-  var template = Handlebars.compile(source);
-
-  // Creo un array vuoto che poi sarà popolato dinamicamente da oggetti
-  var currentMonth = [];
-
-  // inizializzo un ciclo FOR per creare il caledario dinamicamente
-  for (var i = 1; i <= daysMonth; i++) {
-    // Setto il formato dei giorni della settimana
-    days = moment(initialDate).format("dddd");
-    // Creo l'oggetto contenente il numero del giorno corrente e il giorno della settimana
-    var context = {
-      dayNumber: i,
-      DayOfWeek: days,
-    };
-    // Aggiungo un giorno ad ogni giro fino all'ultimo giorno del mese
-    initialDate.add(1,'days');
+  addHolidays(initialDate);
 
 
-    var html = template(context);
 
-    // Inserisco l'oggetto nell'array vuoto creato precedentemente
-    currentMonth.push(context);
 
-    // Stampo i giorni del calendario
-    $('.calendar_container').append(html);
+
+
+
+  // FUNZIONI
+
+  // Funzione per stampare i giorni del mese
+  function displayMonth(initialDate){
+
+    // Recupro il mese corrente
+    var month = moment(initialDate).month();
+
+    // Calcolo quanto giorni ci sono nel mese
+    var daysMonth = moment(month).daysInMonth();
+
+    var year = moment(initialDate).year();
+
+    // console.log(year);
+
+    //Setto il formato del mese da visualizzare nell'html
+
+    var monthInHtml = moment(initialDate).format("MMMM");
+
+    // Lo visualizzo nell'html
+    $('#month_displayed').html(monthInHtml + ' ' + year);
+    $('#month_displayed').attr('current_month', initialDate.format('YYYY-MM-DD'));
+
+    // console.log(monthInHtml);
+
+    // TEMPLATE handlebars
+
+    var source = $('#calendar_template').html();
+    var template = Handlebars.compile(source);
+
+    // inizializzo un ciclo FOR per creare il caledario dinamicamente
+    for (var i = 1; i <= daysMonth; i++) {
+      // Setto il formato dei giorni della settimana
+      days = moment(initialDate).format("dddd");
+      // Creo l'oggetto contenente il numero del giorno corrente e il giorno della settimana
+      var singleDay = moment({
+        dayNumber: i,
+        DayOfWeek: days,
+        month: month,
+        year: year
+      });
+
+      var context = {
+        date: days + ' ' + i + ' ' + moment(month).format('MMMM'),
+      };
+
+      console.log(context);
+      // Aggiungo un giorno ad ogni giro fino all'ultimo giorno del mese
+      var initialDateInFor = initialDate;
+
+      initialDateInFor.add(1,'days');
+
+      // console.log(context);
+
+      var html = template(context);
+
+      // Stampo i giorni del calendario
+      $('.list').append(html);
+
+    }
+
 
   }
 
-  console.log(currentMonth);
+  // Funzione per aggiungere le festività
+  function addHolidays(initialDate){
+    // Faccio la chiamata alla corrispondente API di Boolean per ottenere le festività
+    $.ajax(
+      {
+        url:'https://flynn.boolean.careers/exercises/api/holidays',
+        method: 'GET',
+        data: {
+          year: moment(initialDate).year(),
+          month: moment(initialDate).month(),
+        },
+        // SE LA CHIAMATA HA SUCCESSO:
+        success: function(request){
+          var holidays = request.response;
 
-  nDays = moment(initialDate).format('MMMM dddd');
+          console.log(holidays);
 
-  console.log(initialDate);
+          // Aggiungo le classi create agli elementi per evidenziare ogni festività
+          // utilizzando un ciclo for
 
+          for (var j = 0; j < holidays.length; j++) {
+            var thisHoliday = holidays[j];
 
-  // FACCIO LA CHIAMATA AJAX ALL'API BOOLEAN PER OTTENERE LE FESTIVITA
+            var thisDay = $('.day[data_current_day="'+ thisHoliday.date +'"]');
 
-  var holidays = $.ajax(
-    {
-      url:"https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
-      method: "GET",
+            console.log(thisDay);
 
-      // SE LA CHIAMATA HA SUCCESSO:
+            thisDay.addClass('holiday');
 
-      success: function(request){
+            thisDay.append('-' + thisHoliday.name);
 
-        var dataRequest = request.response;
-        // console.log(dataRequest);
-        var  arrayCalendar = [];
-        var  arrayRequest = [];
-
-        for (var j = 0; j < dataRequest.length; j++) {
-
-          var singleElement = dataRequest[j];
-
-          var dateInRequest = singleElement.date;
-
-          var nameHoliday = singleElement.name;
-
-          var dayInRequest = moment(dateInRequest).day();
-
-          arrayRequest.push(dayInRequest)
-
-          console.log(dayInRequest);
-          console.log(nameHoliday);
-
-          for (var a = 0; a < currentMonth.length; a++) {
-            var currentMonthElement = currentMonth[a];
-            var dayInCurrentMonth = currentMonthElement.dayNumber;
-
-            arrayCalendar.push(dayInCurrentMonth);
-
-            var control = false;
-
-            if (arrayCalendar.includes(dayInRequest)) {
-              var control = true;
-
-
-            }
 
           }
 
-          if (control) {
-            $('.calendar_container').find('li').append(nameHoliday);
-          }
 
-
+        },
+        // SE LA CHIAMATA DOVESSE FALLIRE
+        // visualizzo un errore
+        error: function(){
+          alert("Qualcosa è andato storto");
         }
-
-
-
-
-
-
-        console.log(control);
-        console.log(arrayCalendar);
-
-
-        console.log(dayInCurrentMonth);
-        //
-        // if (control) {
-        //   console.log("sono nella if");
-        // }
-
-
-
-
-
-
-
-
-
-      },
-
-
-      // SE LA CHIAMATA DOVESSE FALLIRE
-      // visualizzo un errore
-      error: function(){
-        alert("Si è verificato un errore");
       }
+    );
 
   }
-  )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 });
